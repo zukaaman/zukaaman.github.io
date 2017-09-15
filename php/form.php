a1edea48c0566bad57d9b11d56096c2236c5f42e
zukaaman <zukaaman@gmail.com>
2017-09-16 01:52:16 +0400

    mail2

--- php/form.php
+++ php/form.php
@@ -5,13 +5,6 @@
         $text = trim(strip_tags($_POST['text']));
 
         mail('zukaaman@gmail.com', 'Письмо с адрес_вашего_сайта', 'Вам написал: '.$name.'<br />Его почта: '.$email.'<br />Его сообщение: '.$text,"Content-type:text/html;charset=windows-1251");
-
-        echo "Ваше сообщение успешно отправлено!<Br> Вы получите ответ в ближайшее время<Br> $back";
-
-        exit;
-    }
-    else {
-        echo "Для отправки сообщения заполните все поля! $back";
         exit;
     }
 ?>
