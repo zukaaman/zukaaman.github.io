d2dca316467baa091fccdd82e8ab25e9e714ae53
zukaaman <zukaaman@gmail.com>
2017-09-18 15:12:46 +0400

    mail5

--- php/form.php
+++ php/form.php
@@ -1,13 +1,13 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['text']) ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-  echo "No arguments Provided!";
-  return false;
-   }
+// if(empty($_POST['name'])      ||
+//    empty($_POST['email'])     ||
+//    empty($_POST['text']) ||
+//    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+//    {
+//   echo "No arguments Provided!";
+//   return false;
+//    }
 
 $name = $_POST['name'];
 $email_address = $_POST['email'];
