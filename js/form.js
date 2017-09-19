d7cbd97f99f0fa5bf5606d88011916772c0391d9
zukaaman <zukaaman@gmail.com>
2017-09-19 16:32:07 +0400

    post

--- js/form.js
+++ js/form.js
@@ -13,21 +13,19 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
+            var message = $("textarea#text").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "../php/form.php",
                 type: "POST",
                 data: {
                     name: name,
-                    phone: phone,
                     email: email,
-                    message: message
+                    message: text
                 },
                 cache: false,
                 success: function() {
