50b9d5cb082eac83c3e6db0601493f015cae132e
zukaaman <zukaaman@gmail.com>
2017-09-21 19:27:59 +0400

    social

--- js/form.js
+++ js/form.js
@@ -1,71 +1,30 @@
-$(function() {
+$(document).on('submit', '#main-form', function(e) {
+    e.preventDefault();
+    var replyto = $('input[name=_replyto]'),
+        replyto_text = replyto.val(),
+        subject = $('input[name=_subject]'),
+        subject_text = subject.val(),
+        body = $('textarea[name=body]'),
+        body_text = body.val();
 
-    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
+    $.ajax({
+        url: "https://formspree.io/addr@example.com",
+        method: "POST",
+        data: {
+            _replyto: replyto_text,
+            _subject: subject_text,
+            message: body_text
         },
-        submitSuccess: function($form, event) {
-            // Prevent spam click and default submit behaviour
-            $("#btnSubmit").attr("disabled", true);
-            event.preventDefault();
-
-            // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var message = $("textarea#text").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-            $.ajax({
-                url: "../php/form.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    email: email,
-                    message: text
-                },
-                cache: false,
-                success: function() {
-                    // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
-
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
-        },
-        filter: function() {
-            return $(this).is(":visible");
-        },
+        dataType: "json",
+        beforeSend: function( xhr ) {
+            spinner.show();
+      }
+    }).done(function(){
+        $('#contacts-success-wrapper').show();
+    }).fail(function() {
+        $('#contacts-error-wrapper').show();
+    }).always(function() {
+        spinner.hide();
+        $('#contacts-wrapper').remove();
     });
-
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
-});
-
-// When clicking on Full hide fail/success boxes
-$('#name').focus(function() {
-    $('#success').html('');
 });
--- php/form.php
+++
@@ -1,8 +0,0 @@
-<?
-if (isset ($_POST['text'])) {
-  mail ("zka18@yandex.ru",
-        "заполнена контактная форма с ".$_SERVER['HTTP_REFERER'],
-        "Имя: ".$_POST['name']."\nEmail: ".$_POST['email']."\nСообщение: ".$_POST['text']);
-  echo ('<p style="color: green">Ваше сообщение получено, спасибо!</p>');
-}
-?>
