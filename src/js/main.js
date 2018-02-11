e381fe7173df5eaf05088b99cdcedf912e13fc5a
zukaaman <zukaaman@gmail.com>
2018-02-11 20:59:05 +0400

    наброски портфолио-сайта

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,18 @@
  * Custom
  */
 
+document.addEventListener("DOMContentLoaded", function(event) {
+//= ../../node_modules/jquery/dist/jquery.js
+
+//= partials/canvas.js
+
+//= partials/main-slider.js
+
 //= partials/scrolling.js
 
 //= partials/toggle.js
 
 //= partials/form.js
+
+//= ../../node_modules/slick-carousel/slick/slick.js
+});
---
+++ src/js/partials/main-slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.main-slider').slick({
+    vertical: true,
+    verticalSwiping: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false
+  });
+});
